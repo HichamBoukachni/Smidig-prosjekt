fc5eed1277437daa49d75fae20b35c9e0efcc101
christina <christinasjowall@outlook.com>
2024-06-03 23:56:18 +0200

    Ikke ferdig:)

--- StartSide(Christina).py
+++ StartSide(Christina).py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, \
-    QComboBox, QSpacerItem, QSizePolicy
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QComboBox, \
+    QPushButton, QSizePolicy, QSpacerItem
+from PyQt5.QtGui import QIcon, QPixmap, QFont
 from PyQt5.QtCore import Qt, QSize
 
 
@@ -9,149 +9,109 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Design for grunnsiden
         self.setWindowTitle("Volatility3")
-        self.setGeometry(100, 100, 800, 400)
+        self.resize(800, 600)
         self.setStyleSheet("background-color:  rgb(18, 25, 33);"
-                           "color: rgb(230, 232, 234)")
-        # self.showMaximized()
+                           "color: rgb(177, 188, 200);")
 
-        # Main layout
-        main_widget = QWidget()
-        self.setCentralWidget(main_widget)
-        main_layout = QVBoxLayout()
-        main_widget.setLayout(main_layout)
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        main_layout = QVBoxLayout(central_widget)
 
-        # Top layout for Dropdown, Profile og Settings
-        top_layout = QHBoxLayout()
-        top_layout.setContentsMargins(0, 0, 0, 0)
+        # Dropdown menu for OS
+        dropdown_layout = QHBoxLayout()
+        dropdown = QComboBox()
+        dropdown.addItems(["Linux", "Windows", "macOS"])
+        dropdown.setStyleSheet("""
+                                QComboBox { 
+                                    background-color: rgb(59, 73, 89);
+                                    color: rgb(230, 232, 234); 
+                                    padding: 5px; 
+                                    font-size: 20px;
+                                    border-style: solid; 
+                                    border-color: rgb(217, 111, 51);
+                                    border-width: 2px;
+                                    border-radius: 10px; 
+                                    width: 150%;
+                                } 
+                                QComboBox::drop-down {
+                                    border-radius: 10px; 
+                                    width: 40%;
+                                }
+                                QComboBox::down-arrow {
+                                    image: url(images/_arrowdownO.png);
+                                    max-width: 150%; 
+                                    max-height: 150%;
+                                }
+                                """)
+        dropdown_layout.addWidget(dropdown, alignment=Qt.AlignLeft)
+        dropdown_layout.addSpacerItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
 
+        # Profile and settings buttons
+        profilesetting_layout = QHBoxLayout()
 
-        # Middle layout for Title, margins og spacing
-        middle_layout = QHBoxLayout()
-        middle_spacer = QWidget()
-        middle_spacer.setFixedHeight(-50)
-        main_layout.addWidget(middle_spacer)
-        middle_layout.setContentsMargins(0, 70, 0, 0)
-        middle_layout.setSpacing(0)
+        profile_btn = QPushButton()
+        profile_btn.setIcon(QIcon("images/_profile.png"))
+        profile_btn.setFixedSize(40, 40)
+        profile_btn.setIconSize(profile_btn.size())
 
-        # Dropdown
-        self.dropdown = QComboBox()
-        self.dropdown.addItems(["Linux", "Windows", "macOS"])
-        self.dropdown.setStyleSheet("""
-                                    QComboBox { 
-                                        background-color: rgb(59, 73, 89);
-                                        color: rgb(230, 232, 234); 
-                                        padding: 5px; 
-                                        font-size: 20px;
-                                        border-style: solid; 
-                                        border-color: rgb(217, 111, 51);
-                                        border-width: 2px;
-                                        border-radius: 10px; 
-                                        width: 150%;
-                                    } 
-                                    QComboBox::drop-down {
-                                        border-radius: 10px; 
-                                        width: 40%;
-                                    }
-                                    QComboBox::down-arrow {
-                                        image: url(icons/arrowdownO.png);
-                                        max-width: 150%; 
-                                        max-height: 150%;
-                                    }
-                                    """)
-        top_layout.addWidget(self.dropdown, alignment=Qt.AlignTop | Qt.AlignLeft)
-        top_layout.addStretch()
+        setting_btn = QPushButton()
+        setting_btn.setIcon(QIcon("images/_settings.png"))
+        setting_btn.setFixedSize(40, 40)
+        setting_btn.setIconSize(setting_btn.size())
 
-        # Profile og Settings buttons
-        self.profile_btn = QPushButton()
-        self.profile_btn.setIcon(QIcon("icons/profile.png"))
-        self.profile_btn.setIconSize(QSize(40, 40))
-        self.profile_btn.setStyleSheet("QPushButton { background-color: ; border: none;}")
-        top_layout.addWidget(self.profile_btn, alignment=Qt.AlignTop)
+        profilesetting_layout.addWidget(profile_btn)
+        profilesetting_layout.addWidget(setting_btn)
 
-        self.settings_btn = QPushButton()
-        self.settings_btn.setIcon(QIcon("icons/settings.png"))
-        self.settings_btn.setIconSize(QSize(40, 40))
-        self.settings_btn.setStyleSheet("QPushButton { background-color: ; border: none; }")
-        top_layout.addWidget(self.settings_btn, alignment=Qt.AlignTop)
+        dropdown_layout.addLayout(profilesetting_layout)
+        main_layout.addLayout(dropdown_layout)
 
-        main_layout.addLayout(top_layout)
+        # Spacer to push title and icon upwards
+        main_layout.addSpacerItem(QSpacerItem(20, 60, QSizePolicy.Minimum, QSizePolicy.Expanding))
 
-        # Title
-        self.titleVol = QLabel("Volatility3 ")
-        self.titleVol.setStyleSheet("color: rgb(177, 188, 200); font-size: 80px; font-weight: bold;")
-        self.titleVol.setAlignment(Qt.AlignCenter)
+        # Title and Icon
+        title_layout = QHBoxLayout()
+        title_layout.addSpacerItem(QSpacerItem(40, 60, QSizePolicy.Expanding, QSizePolicy.Minimum))
 
-        self.titleBy = QLabel("by")
-        self.titleBy.setStyleSheet("color: rgb(177, 188, 200); font-size: 40px; font-weight: bold; margin-top: 34%;")
-        self.titleBy.setAlignment(Qt.AlignCenter)
+        title = QLabel("<span style='font-size:80px'>Volatility3  </span> <span style='font-size:40px'>by</span>")
+        title.setFont(QFont("Arial"))
+        title.setStyleSheet("color: rgb(177, 188, 200); font-weight: bold")
+        title_layout.addWidget(title)
 
-        # Legger til Title til middle layout
-        middle_layout.addWidget(self.titleVol)
-        middle_layout.addWidget(self.titleBy)
+        # Icon
+        icon_label = QLabel()
+        icon_label.setFixedSize(50, 50)
+        icon_label.setStyleSheet("image: url(images/_mnemoniclogo.png);")
+        title_layout.addWidget(icon_label)
 
-        main_layout.addLayout(middle_layout)
-        main_layout.setAlignment(middle_layout, Qt.AlignTop | Qt.AlignHCenter)
+        title_layout.addSpacerItem(QSpacerItem(40, 60, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        main_layout.addLayout(title_layout)
 
-        # Logo
-        self.logo = QLabel()
-        self.logo.setStyleSheet("width: ;")
-        self.logo.setAlignment(Qt.AlignCenter)
-        self.logo.setPixmap(QPixmap("icons/mnemoniclogo.png"))
+        # Spacer between title and buttons
+        main_layout.addSpacerItem(QSpacerItem(20, 100, QSizePolicy.Minimum, QSizePolicy.Minimum))
 
-        middle_layout.addWidget(self.logo)
-        main_layout.addSpacerItem(QSpacerItem(40, 40, QSizePolicy.Maximum, QSizePolicy.Expanding))
+        # Buttons container and layout
+        icon_layout = QHBoxLayout()
+        icon_container = QWidget()
+        icon_container.setLayout(icon_layout)
+        icon_container.setFixedSize(950, 200)
+        main_layout.addWidget(icon_container, alignment=Qt.AlignCenter)
 
-        # Icons layout
-        icons_layout = QHBoxLayout()
-        icons_container = QWidget()
-        icons_container.setLayout(icons_layout)
-        main_layout.addWidget(icons_container, alignment=Qt.AlignCenter)
+        icon_container.setStyleSheet("""
+                                             border-style: solid;
+                                             border-width: 2px;
+                                             border-radius: 15px;
+                                             border-color: rgb(217, 111, 51);
+                                             background-color: rgb(28, 37, 48);
+                                             width: 300%;
+                                             height: 90%;
+                                        """)
 
-        icons_container.setStyleSheet("""border-style: solid;
-                                      border-width: 2px;
-                                      border-radius: 15px;
-                                      border-color: rgb(217, 111, 51);
-                                      background-color: rgb(28, 37, 48);
-                                      width: 300%;
-                                      height: 90%;
-                                      """)
-        icons_layout.setSpacing(30)
-
-        """
-        button_style = 
-                        QPushButton { 
-                            background-color: rgb(28, 37, 48); 
-                            border-style: solid; 
-                            border-width: 2px;
-                            border-radius: 10px;
-                            broder-color: rgb(217, 111, 51);
-                            padding: 20px; 
-                        } 
-                        QPushButton:hover { 
-                            background-color: rgb(28, 37, 48); 
-                        }
-
-        # Cmd button
-        cmd_btn = QPushButton("Command")
-        cmd_btn.setStyleSheet(button_style)
-
-        # Plugin button
-        pluginupload_btn = QPushButton("Plugin", "Upload")
-        # pluginupload_btn.setIcon(QPixmap("icons/uploadgray.png"))
-        pluginupload_btn.setStyleSheet(button_style)
-
-        # Result button
-        result_btn = QPushButton("Result")
-        result_btn.setStyleSheet(button_style)
-        """
-
-        # Icons
+        # Buttons
         icons = [
-            (QPixmap("icons/cmd.png"), "Command"),
-            (QPixmap("icons/plugin.png"), "Plugin"),
-            (QPixmap("icons/results.png"), "Result")
+            (QPixmap("images/_cmd.png"), "Command"),
+            (QPixmap("images/_plugin.png"), "Plugin"),
+            (QPixmap("images/_results.png"), "Result")
         ]
 
         for icon, tooltip in icons:
@@ -160,24 +120,61 @@ class MainWindow(QMainWindow):
             button.setIconSize(QSize(90, 90))
             button.setToolTip(tooltip)
             button.setStyleSheet("""
-                                QPushButton { 
+                                 QPushButton { 
                                     background-color: rgb(59, 73, 89); 
                                     border-style: solid; 
                                     border-width: 2px;
                                     border-radius: 15px;
                                     broder-color: rgb(217, 111, 51);
-                                    margin: 20%;
-                                    padding: 10%; 
+                                    margin: 10%;
+                                    padding: 50%; 
                                     width: 150%
-                                } 
-                                QPushButton:hover { 
+                                 } 
+                                 QPushButton:hover { 
                                     background-color:  rgb(28, 37, 48); 
-                                }
-                                """)
-            icons_layout.addWidget(button)
+                                 }
+                                 """)
 
+            icon_layout.addWidget(button)
 
+        # Spacer to push buttons upwards
+        main_layout.addSpacerItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
 
+        """
+        # Buttons second version
+        cmd_btn = QPushButton()
+        cmd_btn.setIcon(QIcon("images/_cmd.png"))
+        cmd_btn.setFixedSize(100, 100)
+        cmd_btn.setIconSize(cmd_btn.size())
+        cmd_btn.setStyleSheet("background-color: rgb(59, 73, 89); 
+                                    border-style: solid; 
+                                    border-width: 2px;
+                                    border-radius: 15px;
+                                    broder-color: rgb(217, 111, 51);")
+        icon_layout.addWidget(cmd_btn)
+
+        plugin_btn = QPushButton()
+        plugin_btn.setIcon(QIcon("images/_plugin.png"))
+        plugin_btn.setFixedSize(100, 100)
+        plugin_btn.setIconSize(plugin_btn.size())
+        plugin_btn.setStyleSheet("background-color: rgb(59, 73, 89); 
+                                    border-style: solid; 
+                                    border-width: 2px;
+                                    border-radius: 15px;
+                                    broder-color: rgb(217, 111, 51);")
+        icon_layout.addWidget(plugin_btn)
+
+        result_btn = QPushButton()
+        result_btn.setIcon(QIcon("images/_results.png"))
+        result_btn.setFixedSize(100, 100)
+        result_btn.setIconSize(result_btn.size())
+        result_btn.setStyleSheet("background-color: rgb(59, 73, 89); 
+                                    border-style: solid; 
+                                    border-width: 2px;
+                                    border-radius: 15px;
+                                    broder-color: rgb(217, 111, 51);")
+        icon_layout.addWidget(result_btn)
+        """
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
